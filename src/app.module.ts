e5b9a27472a164e5f137c8bbc526c50f841c6e75
GW <kowonp@gmail.com>
2022-02-09 02:39:16 +0900

    Add GraphQL Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: true,
+    }),
+    RestaurantsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
