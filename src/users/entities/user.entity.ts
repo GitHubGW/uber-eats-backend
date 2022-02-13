a95dfe7fbcd5a8a9ecbd6bade2d60f4673c767d8
GW <kowonp@gmail.com>
2022-02-14 02:40:59 +0900

    Define TypeScript enum, GraphQL enum

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,20 +1,31 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { IsEmail, IsString } from 'class-validator';
 import { Common } from 'src/common/entities/common.entity';
 import { Column, Entity } from 'typeorm';
 
-type Role = 'Owner' | 'Customer' | 'DeliveryMan';
+enum Role {
+  Owner,
+  Customer,
+  DeliveryMan,
+}
 
+registerEnumType(Role, { name: 'Role' });
+
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends Common {
+  @Field((type) => String)
   @Column()
   @IsEmail()
   email: string;
 
+  @Field((type) => String)
   @Column()
   @IsString()
   password: string;
 
-  @Column()
+  @Field((type) => Role)
+  @Column({ type: 'enum', enum: Role })
   role: Role;
 }
